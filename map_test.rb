6167ddda11b7d56026ca75e014c61c68f24cd944
joshuacronemeyer <joshuacronemeyer@6aeef455-2d47-0410-8cde-010f0c17ee65>
2008-04-05 23:28:17 +0000

    fixed the test

--- map_test.rb
+++ map_test.rb
@@ -4,6 +4,7 @@ require 'tile'
 require 'rubygems'
 require 'mocha'
 require 'gosu'
+require 'chipmunk'
 include Gosu
 class MapTest < Test::Unit::TestCase
 
@@ -12,7 +13,8 @@ class MapTest < Test::Unit::TestCase
     Image.expects(:load_tiles)
     lines = ["GGEE\n", "EEGG\n", "GGGG\n"]
     File.expects(:readlines).returns(lines)
-    @map = Map.new(:window, "filename")
+    space = stub(:add_body=>0, :add_shape=>0)
+    @map = Map.new(:window, space, "filename")
     @tiles = get @map, :@tiles
   end
   
@@ -26,17 +28,22 @@ class MapTest < Test::Unit::TestCase
   end
   
   def test_tiles_get_correct_offsets
+    has_offset_between_elements(60, @tiles)
+  
     first_column = @tiles.first
-    assert_equal 0, get(first_column.first, :@x)
-    assert_equal 0, get(first_column.first, :@y)
-    assert_equal 60, get(first_column[1], :@y)
-    assert_equal 120, get(first_column[2], :@y)
-    
+    assert_equal 0, get(first_column.first, :@shape).body.p.x
     second_column = @tiles[1]
-    assert_equal 60, get(second_column.first, :@x)
-    assert_equal 0, get(second_column.first, :@y)
-    assert_equal 60, get(second_column[1], :@y)
-    assert_equal 120, get(second_column[2], :@y)
+    assert_equal 60, get(second_column.first, :@shape).body.p.x
+  end
+
+  def has_offset_between_elements(offset, tiles)
+    tiles.each do |column|
+      column.each_cons(2) do |element_y1, element_y2|
+        location_y1 = get(element_y1, :@shape).body.p.y
+        location_y2 = get(element_y2, :@shape).body.p.y
+        assert_equal offset, (location_y2 - location_y1)
+      end
+    end
   end
   
   def test_tile_type_sets_correctly
